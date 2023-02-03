b661b586f0ae30b362bcc078ea7ffa1e7cfd8dc4
Its-A-Mia <miapurdun@gmail.com>
2023-02-02 22:08:20 -0600

    finished colors file and created tests. All tests passed

---
+++ src/__tests__/design-system/utils/colors.test.ts
@@ -0,0 +1,29 @@
+import {
+  getActiveColor,
+  getBackgroundColor,
+  getBorderColor,
+  getColor,
+  getHoverBackgroundColor,
+} from '../../../design-system/utils/colors';
+
+describe('colors', () => {
+  it('can return a string defining color as css', () => {
+    expect(getColor('white')).toBe('color: #ffffff');
+  });
+
+  it('can return a string defining an active elements color as css', () => {
+    expect(getActiveColor('white')).toBe('color: #ffffff');
+  });
+
+  it('can return a string defining background-color as css', () => {
+    expect(getBackgroundColor('light')).toBe('background-color: #f2f2f2');
+  });
+
+  it('can return a string defining background-color if hovering an element as css', () => {
+    expect(getHoverBackgroundColor('light')).toBe('background-color: #f2f2f2');
+  });
+
+  it('can return a string defining border-color as css', () => {
+    expect(getBorderColor('blastRedPrimary')).toBe('border-color: #c83728');
+  });
+});
