8dc7d0527fa8d138cf4152d519f30fb2525b5f17
Its-A-Mia <miapurdun@gmail.com>
2023-02-06 20:10:48 -0600

    added context to test

--- src/__tests__/design-system/utils/colors.test.ts
+++ src/__tests__/design-system/utils/colors.test.ts
@@ -6,6 +6,19 @@ import {
   getHoverBackgroundColor,
 } from '../../../design-system/utils/colors';
 
+// colors are as followed:
+//   primary: '#e56C42'
+//   secondary: '#c83728'
+//   tertiary: '#CDD7E8'
+//   success: '#146217'
+//   danger: '#c83728'
+//   warning: '#ffc107'
+//   info: '#0f49A7'
+//   light: '#f2f2f2'
+//   dark: '#333333'
+//   white: '#ffffff'
+//   black: '#000000'
+
 describe('colors', () => {
   it('can return a string defining color as css', () => {
     expect(getColor('white')).toBe('color: #ffffff');
